c05032ef6ef77737a8231a725ef9b9a6b7a0b0cc
Isa Sunasra <isa.rizwan@vit.edu.in>
2022-11-29 01:38:42 +0530

    Updated env variables

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-
 export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
@@ -14,6 +13,5 @@ const options = {
 
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-
   return data;
 };
